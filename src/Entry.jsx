39726170c485a1e4a44d6c6b9eff2a03d5e0ab8a
oussama1603 <oussamadz1603@gmail.com>
2026-08-08 14:34:03 +0100

    ading props

--- src/Entry.jsx
+++ src/Entry.jsx
@@ -6,13 +6,14 @@ export default function Entry(props){
       <div className="main-image-container">
          <img 
          className ='main-image' 
-         
+         src = {props.img.src}
+         alt= {props.img.alt}
           ></img>
       </div>
       <div className="journal-info">
         <FaMapMarkerAlt size={20} color="#F55A5A" className = "marker-icon" />
         <span>{props.title}</span>
-        <a target="blanck" href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on google maps</a>
+        <a target="blanck" href={props.link}>View on google maps</a>
         <h2>{props.place}</h2>
         <p>{props.date}</p>
         <p>{props.description}</p>
