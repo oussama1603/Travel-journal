82a1794e7488293ea10c1b3548b72a5a3b89267e
oussama1603 <oussamadz1603@gmail.com>
2026-08-08 14:27:26 +0100

    !

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ export default function App(){
   return(
     <>
       <Header />
-      <Entry /> 
+      <Entry 
+      img = {{
+        src="src/assets/japan.webp",
+        alt="japan-image"
+      }} 
+      title  = 'Japan'
+      place  = 'Mount Fuji'
+      date = '12 Jan, 2021 - 24 Jan, 2021'
+      description = 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>'
+      /> 
     </>
   )
 }
--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,21 +1,21 @@
 import { FaMapMarkerAlt } from "react-icons/fa";
 
-export default function Entry(){
+export default function Entry(props){
   return(
     <article className="journal-entry">
       <div className="main-image-container">
          <img 
          className ='main-image' 
-         src="src/assets/japan.webp" 
-         alt="" ></img>
+         
+          ></img>
       </div>
       <div className="journal-info">
         <FaMapMarkerAlt size={20} color="#F55A5A" className = "marker-icon" />
-        <span>Japan</span>
+        <span>{props.title}</span>
         <a target="blanck" href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on google maps</a>
-        <h2>Mount Fuji</h2>
-        <p>12 Jan, 2021 - 24 Jan, 2021</p>
-        <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        <h2>{props.place}</h2>
+        <p>{props.date}</p>
+        <p>{props.description}</p>
       </div>
     </article>
   )
